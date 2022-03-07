c6bf44d757feef042923c42971fdd1e6f429fca6
MinChul-Son <smc5236@gmail.com>
2022-03-07 17:41:42 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/minchul/springkafkastudy/SpringKafkaStudyApplication.java
@@ -0,0 +1,14 @@
+package com.minchul.springkafkastudy;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class SpringKafkaStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaStudyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/minchul/springkafkastudy/SpringKafkaStudyApplicationTests.java
@@ -0,0 +1,11 @@
+package com.minchul.springkafkastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaStudyApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
