311c9d2d281bea9a787ed0e4619efe63f49b1774
MinChul-Son <smc5236@gmail.com>
2022-03-08 22:50:25 +0900

    ✨ feat: Consumer 생성
    - 앞서 만든 2개의 토픽을 consume

---
+++ src/main/java/com/minchul/springkafkastudy/consumer/HelloConsumer.java
@@ -0,0 +1,18 @@
+package com.minchul.springkafkastudy.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloConsumer {
+
+    @KafkaListener(id = "kafka3-id", topics = "topic3")
+    public void listen(String message) {
+        System.out.println(message);
+    }
+
+    @KafkaListener(id = "kafka3-bytes-id", topics = "topic3-bytes")
+    public void listenBytes(String message) {
+        System.out.println(message);
+    }
+}
