4aef2dc8c9985297df6756a17313b346ea61200b
MinChul-Son <smc5236@gmail.com>
2022-03-08 17:55:04 +0900

    ⚡️ refactor: 원활한 학습을 위해 이전 코드 주석 처리

--- src/main/java/com/minchul/springkafkastudy/SpringKafkaStudyApplication.java
+++ src/main/java/com/minchul/springkafkastudy/SpringKafkaStudyApplication.java
@@ -27,23 +27,23 @@ public class SpringKafkaStudyApplication {
 //        };
 //    }
 
-    @Bean
-    public ApplicationRunner runner(AdminClient adminClient) {
-        return args -> {
-            ListTopicsResult listTopicsResult = adminClient.listTopics();
-            KafkaFuture<Map<String, TopicListing>> mapKafkaFuture = listTopicsResult.namesToListings();
-            Map<String, TopicListing> stringTopicListingMap = mapKafkaFuture.get();
-            for (String topicName : stringTopicListingMap.keySet()) {
-                System.out.println("topicName =" + topicName);
-                TopicListing topicListing = stringTopicListingMap.get(topicName);
-                System.out.println(topicListing);
-
-                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName))
-                                                                                     .all()
-                                                                                     .get();
-                System.out.println(description);
-                adminClient.deleteTopics(Collections.singleton(topicName));
-            }
-        };
-    }
+//    @Bean
+//    public ApplicationRunner runner(AdminClient adminClient) {
+//        return args -> {
+//            ListTopicsResult listTopicsResult = adminClient.listTopics();
+//            KafkaFuture<Map<String, TopicListing>> mapKafkaFuture = listTopicsResult.namesToListings();
+//            Map<String, TopicListing> stringTopicListingMap = mapKafkaFuture.get();
+//            for (String topicName : stringTopicListingMap.keySet()) {
+//                System.out.println("topicName =" + topicName);
+//                TopicListing topicListing = stringTopicListingMap.get(topicName);
+//                System.out.println(topicListing);
+//
+//                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName))
+//                                                                                     .all()
+//                                                                                     .get();
+//                System.out.println(description);
+//                adminClient.deleteTopics(Collections.singleton(topicName));
+//            }
+//        };
+//    }
 }
--- src/main/java/com/minchul/springkafkastudy/configuration/KafkaTopicConfiguration.java
+++ src/main/java/com/minchul/springkafkastudy/configuration/KafkaTopicConfiguration.java
@@ -10,25 +10,25 @@ import org.springframework.kafka.core.KafkaAdmin;
 
 @Configuration
 public class KafkaTopicConfiguration {
-    @Bean
-    public NewTopic topic() {
-        return TopicBuilder.name("from_topic_builder").build();
-    }
-
-    @Bean
-    public KafkaAdmin.NewTopics topics() {
-        return new KafkaAdmin.NewTopics(
-            TopicBuilder.name("from-topic-builder1").build(),
-            TopicBuilder.name("from-topic-builder2")
-                .partitions(3)
-                .replicas(1)
-                .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(1000 * 60 * 60)) // Retention 시간을 60분으로 설정
-                        .build()
-        );
-    }
-
-    @Bean
-    public AdminClient adminClient(KafkaAdmin kafkaAdmin) {
-        return AdminClient.create(kafkaAdmin.getConfigurationProperties());
-    }
+//    @Bean
+//    public NewTopic topic() {
+//        return TopicBuilder.name("from_topic_builder").build();
+//    }
+//
+//    @Bean
+//    public KafkaAdmin.NewTopics topics() {
+//        return new KafkaAdmin.NewTopics(
+//            TopicBuilder.name("from-topic-builder1").build(),
+//            TopicBuilder.name("from-topic-builder2")
+//                .partitions(3)
+//                .replicas(1)
+//                .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(1000 * 60 * 60)) // Retention 시간을 60분으로 설정
+//                        .build()
+//        );
+//    }
+//
+//    @Bean
+//    public AdminClient adminClient(KafkaAdmin kafkaAdmin) {
+//        return AdminClient.create(kafkaAdmin.getConfigurationProperties());
+//    }
 }
