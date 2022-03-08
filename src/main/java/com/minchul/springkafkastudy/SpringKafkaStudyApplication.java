487bb45a3d35b292d684a73e5e0179e361508094
MinChul-Son <smc5236@gmail.com>
2022-03-08 11:38:51 +0900

    ✨ feat: AdminClient 사용해보기
    - AdminClient를 사용해 해당 토픽에 대한 정보를 가져올 수 있음
    - AdminClient를 사용해 생성된 토픽을 삭제할 수 있음

--- src/main/java/com/minchul/springkafkastudy/SpringKafkaStudyApplication.java
+++ src/main/java/com/minchul/springkafkastudy/SpringKafkaStudyApplication.java
@@ -1,5 +1,13 @@
 package com.minchul.springkafkastudy;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.common.KafkaFuture;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +21,29 @@ public class SpringKafkaStudyApplication {
     }
 
 //    @Bean
-    public ApplicationRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
+//    public ApplicationRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
+//        return args -> {
+//            kafkaTemplate.send("quickstart-events", "hello-world");
+//        };
+//    }
+
+    @Bean
+    public ApplicationRunner runner(AdminClient adminClient) {
         return args -> {
-            kafkaTemplate.send("quickstart-events", "hello-world");
+            ListTopicsResult listTopicsResult = adminClient.listTopics();
+            KafkaFuture<Map<String, TopicListing>> mapKafkaFuture = listTopicsResult.namesToListings();
+            Map<String, TopicListing> stringTopicListingMap = mapKafkaFuture.get();
+            for (String topicName : stringTopicListingMap.keySet()) {
+                System.out.println("topicName =" + topicName);
+                TopicListing topicListing = stringTopicListingMap.get(topicName);
+                System.out.println(topicListing);
+
+                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName))
+                                                                                     .all()
+                                                                                     .get();
+                System.out.println(description);
+                adminClient.deleteTopics(Collections.singleton(topicName));
+            }
         };
     }
 }
