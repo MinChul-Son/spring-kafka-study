71145030cf27b326418d100993ac099e5f80d8b8
MinChul-Son <smc5236@gmail.com>
2022-03-14 16:41:46 +0900

    ✨ feat: `Header`를 사용
    - `@Header`를 통한 메타 데이터 활용

--- src/main/java/com/minchul/springkafkastudy/consumer/HelloConsumer2.java
+++ src/main/java/com/minchul/springkafkastudy/consumer/HelloConsumer2.java
@@ -2,6 +2,8 @@ package com.minchul.springkafkastudy.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +13,18 @@ public class HelloConsumer2 {
      * clientIdPrefix -> 콘솔창에 출력될 clientId의 prefix를 설정하는 옵션
      */
     @KafkaListener(id = "test4-listener", topics = "test4-listener", concurrency = "2", clientIdPrefix = "listener_id")
-    public void listen(String message, ConsumerRecordMetadata metadata) {
+    public void listen(String message, ConsumerRecordMetadata metadata,
+                       @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp,
+                       @Header(KafkaHeaders.OFFSET) long offset,
+                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         System.out.println("Listener message= " + message);
         System.out.println("metadata.offset= " + metadata.offset());
         System.out.println("metadata.topic= " + metadata.topic());
         System.out.println("metadata.timestamp= " + metadata.timestamp());
+
+        System.out.println("header.offset= " + offset);
+        System.out.println("header.topic= " + topic);
+        System.out.println("header.timestamp= " + timestamp);
     }
 
 }
